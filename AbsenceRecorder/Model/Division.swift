fc451229d7640545d412071b4d8785bedc15bc43
Saahil <bahadur.s@etoncollege.org.uk>
2021-03-01 19:41:45 +0000

    added student and division classes

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Rakesh Bahadur on 01/03/2021.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
---
+++ AbsenceRecorder/Model/Student.swift
@@ -0,0 +1,20 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by Rakesh Bahadur on 01/03/2021.
+//
+
+import Foundation
+
+class Student {
+    let forename: String
+    let surname:String
+    let birthday: Date
+    
+    init(forename: String, surname: String, birthday: Date) {
+        self.forename = forename
+        self.surname = surname
+        sself.birthday = birthday
+    }
+}
