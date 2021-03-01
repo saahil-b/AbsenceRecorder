fd38b867ed1a2d0e6fd7cb3a78b7bd5dc3e2d8ef
Saahil <bahadur.s@etoncollege.org.uk>
2021-03-01 19:00:43 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Rakesh Bahadur on 01/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
